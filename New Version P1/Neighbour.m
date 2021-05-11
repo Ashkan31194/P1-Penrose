10a79c7530180e04dce04e948ba8e8e6c8b71a3f
Ashkan31194 <83088850+Ashkan31194@users.noreply.github.com>
2021-05-11 14:54:48 +0430

    Update Neighbour.m

--- New Version P1/Neighbour.m
+++ New Version P1/Neighbour.m
@@ -1,4 +1,4 @@
-function [ Neigh ] = Neighbour( t,L_d,L_u )
+function [ Neigh ] = Neighbour( t )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 n=size(t,1);
@@ -7,10 +7,5 @@ v=[1 -1 zeros(1,n-2)];
 P=unique(perms(v),'rows');
 dNeigh=P;
 Neigh=Neigh+dNeigh;
-M=max(Neigh,[],2);
-m=min(Neigh,[],2);
-L1=find(M>L_u);
-L2=find(m<L_d);
-LL=union(L1,L2);
-Neigh(LL,:)=[];
 end
+

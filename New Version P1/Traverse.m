3b6956fd4bf6f9aa643f495e7decfd6437812d6e
Ashkan31194 <83088850+Ashkan31194@users.noreply.github.com>
2021-05-11 14:55:24 +0430

    Update Traverse.m

--- New Version P1/Traverse.m
+++ New Version P1/Traverse.m
@@ -1,18 +1,20 @@
-function [ List1 ] = Traverse( S,T,n,L_d,L_u,xv,yv,List)
+function [ List1 ] = Traverse( S,T,D,n,Lx_d,Lx_u,Ly_d,Ly_u,xv,yv,List)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 List1=[List;S'];
 size(List1,1)
-Neigh=Neighbour( S,L_d,L_u );
+Neigh=Neighbour(S);
 
 for i=1:size(Neigh,1)
     S=Neigh(i,:)';
-    if nnz(ismember(List1(:,1:n),S','rows'))==0
-            QS=T'*S;
-             in = inpolygon(QS(1),QS(2),xv,yv);
-             if in==1
-                 List1=Traverse( S,T,n,L_d,L_u,xv,yv,List1);
-             end
+    QS=T'*S;
+    QSS=D'*S;
+    if QSS(1)>Lx_d && QSS(1)<Lx_u && QSS(2)>Ly_d && QSS(2)<Ly_u
+        if nnz(ismember(List1(:,1:n),S','rows'))==0
+                in = inpolygon(QS(1),QS(2),xv,yv);
+                    if in==1
+                         List1=Traverse( S,T,D,n,Lx_d,Lx_u,Ly_d,Ly_u,xv,yv,List1);
+                    end
+        end
     end
-end
 end
